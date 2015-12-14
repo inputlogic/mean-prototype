dc108f99e4ae458dc64fc5b879367ce733d4a5bc
Adrian Unger <dev@staydecent.ca>
2015-12-14 14:54:43 -0800

    Add base build scripts and dependencies.

---
+++ client/what.js
@@ -0,0 +1,4 @@
+
+function what(arg) {
+  console.log(arg);
+}
---
+++ scripts/client.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+var path = require('path');
+var Orchestrator = require('orchestrator');
+var argv = require('yargs').argv;
+var task = argv._[0] || 'default';
+var args = argv._.slice(1);
+var opts = {env: Object.assign(process.env, {PATH: process.env.PATH})};
+var tasks = new Orchestrator();
+
+// Define our tasks
+
+tasks.add('lint', function(done) {
+  var exec = require('child_process').exec;
+  var cmd = 'jshint client/**';
+  exec(cmd, opts, cb.bind(null, done));
+});
+
+// Run the specified task
+tasks.start(task, function(err) {
+  if (err) {
+    console.error(task, err); 
+  } else {
+    console.log(task, 'completed!');
+  }
+});
+
+function cb(done, err, stdout) {
+  if (err) {
+    console.error(task, stdout); 
+  } else {
+    console.log(stdout);
+  }
+  done(err);
+}
