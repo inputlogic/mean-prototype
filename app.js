004e05aee76cc15de1073aaf437b87328947e169
Derek Li <derek.li@mail.utoronto.ca>
2015-12-15 10:28:30 -0800

    changed config.json to config.js and added password field in users table

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // Place database connection values in this file
-app.config = require('./config.json');
+app.config = require('./config.js');
 
 var knex = require('knex')({
   client: 'mysql2',
--- models/users.js
+++ models/users.js
@@ -4,5 +4,6 @@ module.exports = function(table) {
   table.increments();
   table.string('name').notNullable();
   table.string('email').notNullable();
+  table.string('password').notNullable();
   table.timestamps();
 };
