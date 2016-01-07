77757807a393241ead00ad783367f45947d5172b
Gavin Vickery <gavin@geekforbrains.com>
2016-01-07 13:46:54 -0800

    Updating user model as example of new setup.

--- modules/users/model.js
+++ modules/users/model.js
@@ -1,9 +1,21 @@
-// Example "users" model
+module.exports = {
 
-module.exports = function(table) {
-  table.increments();
-  table.string('name').notNullable();
-  table.string('email').notNullable();
-  table.string('password').notNullable();
-  table.timestamps();
+  /**
+   * Creates the table. Called automatically on app startup.
+   */
+  init: function init(table) {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('password').notNullable();
+    table.timestamps();
+  },
+
+  findOne: function findOne(userId, done) {
+    return done(new Error('not implemented')); 
+  },
+
+  findAll: function findAll(done) {
+    return done(new Error('not implemented')); 
+  }
 };
