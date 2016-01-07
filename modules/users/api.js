5a003db9851f020b64b0d6b17a60ea2a9cf90f5e
Adriaan Mulder <adriaan.mulder@gmail.com>
2016-01-07 10:51:57 -0800

    reorganize module.exports

--- modules/users/api.js
+++ modules/users/api.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var userMiddleware = require('./middleware');
 
+module.exports = router;
+
 router.use(userMiddleware.foo);
 
 router.get('/', function(req, res) {
   res.json({message: 'It worked!'});
 });
-
-module.exports = router;
--- modules/users/controller.js
+++ modules/users/controller.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+module.exports = router;
+
 router.get('/index', function(req, res) {
   res.render('users/index.html', {foo: "bar"});
 });
-
-module.exports = router;
