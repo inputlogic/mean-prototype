8f232eb901bb1f1ad9cf5d8198ad31c3a0f78c93
Gavin Vickery <gavin@geekforbrains.com>
2016-01-11 11:32:55 -0800

    Adding comment for winston usage.

--- libs/helpers/winston.js
+++ libs/helpers/winston.js
@@ -1,5 +1,10 @@
 var winston = require('winston');
 
+/**
+ * This is attached to the app global at app.log. Use that instead.
+ *
+ * Ex: app.log.info('foo');
+ */
 module.exports = (function logger() {
 	if(logger.instance == undefined) {
 		logger.instance = new winston.Logger({
