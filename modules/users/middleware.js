d756d8626ec49cd5444b2636c8caf2a2765e9a88
Adriaan Mulder <adriaan.mulder@gmail.com>
2016-01-11 11:25:22 -0800

    switch console log to app log

--- modules/users/middleware.js
+++ modules/users/middleware.js
@@ -1,6 +1,6 @@
 module.exports = {
   foo: function(req, res, next) {
-    console.log('running [foo] middleware at modules/users/middleware.js');
+    app.log.info('running [foo] middleware at modules/users/middleware.js');
     return next();
   }
 }
