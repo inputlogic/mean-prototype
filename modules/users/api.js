869a47761f457550baf5f1b5751ee5ba2f7974e6
Gavin Vickery <gavin@geekforbrains.com>
2016-01-27 11:37:00 -0800

    Cleaning up users model and api routes.

--- modules/users/api.js
+++ modules/users/api.js
@@ -4,49 +4,49 @@ var router = express.Router();
 
 module.exports = router;
 
-router.post('/create', function(req, res) {
-  app.models.users.create(req.body, function(err, rows) {
+router.post('/create', createUser);
+router.post('/login', passport.authenticate('local'), login);
+router.post('/logout', logout);
+router.get('/me', getCurrentUser);
+router.put('/me', updateCurrentUser);
+router.delete('/me', deleteCurrentUser);
+
+function createUser(req, res) {
+  app.models.users.insert(req.body, function(err, rows) {
     if (err) {
       return res.status(err.status).send(err);
     }
     return res.json({user_id: rows[0]});
   });
-});
+}
 
-router.post('/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    return res.send('success');
-  }
-);
+function login(req, res) {
+  return res.send('success');
+}
 
-router.post('/logout', function(req, res){
-  if(!req.user) return res.sendStatus(401);
-
+function logout(req, res){
+  if (!req.user) return res.sendStatus(401);
   req.logout();
-  res.json(req.user);
-});
+  return res.json(req.user);
+}
 
-router.get('/me', function(req, res) {
+function getCurrentUser(req, res) {
 	return (!req.user) ? res.sendStatus(401) : res.json(req.user);
-});
+}
 
-router.put('/me', function(req, res){
-	if(!req.user) return res.sendStatus(401);
-
+function updateCurrentUser(req, res) {
+	if (!req.user) return res.sendStatus(401);
 	app.models.users.update(req.user.id, req.body, function(err, result){
 		return (err) ? res.abort(500) : res.json(result); //TODO: should result be user?
 	})
-});
+}
 
-router.delete('/me', function(req, res) {
-	if(!req.user) return res.sendStatus(401);
+function deleteCurrentUser(req, res) {
+	if (!req.user) return res.sendStatus(401);
 
-	app.models.users.delete(req.user.id, function(err, result) {
-		if (err) {
-      return res.abort(500)
-    }
+  app.models.users.removeById(req.user.id, function(err, result) {
+    if (err) return res.abort(500);
     req.logout();
-    res.json(req.user);
-	});
-});
+    return res.json(req.user);
+  });
+}
